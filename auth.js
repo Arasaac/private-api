1816052e38cf592b0d1ee0bca8014338add1ba49
juanda <juandacorreo@gmail.com>
2023-05-24 13:41:44 +0200

    fix auth url

--- auth.js
+++ auth.js
@@ -13,7 +13,7 @@ const logger = require('./utils/logger')
 
 passport.use(
   new Strategy(async (token, cb) => {
-    const url = `http://auth/api/tokeninfo?access_token=${token}`
+    const url = `http://services_auth/api/tokeninfo?access_token=${token}`
     try {
       if (!token) {
         logger.debug('Authentication failed: No token!!')
